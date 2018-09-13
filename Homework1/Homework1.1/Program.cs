0c06dd77c3653ac1a1b80f256c52716e81d7cb9f
AGYagy <43157278+AGYagy@users.noreply.github.com>
2018-09-13 22:21:10 +0800

    Add files via upload

---
+++ Homework1/Homework1.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Homework1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            string a = Console.ReadLine();
+            Console.Write("b=");
+            string b = Console.ReadLine();
+            double x = Convert.ToDouble(a);
+            double y = Convert.ToDouble(b);
+            Console.WriteLine("a*b=" + (x * y));
+        }
+    }
+}
---
+++ Homework1/Homework1.2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework1._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b;
+            a = Convert.ToInt32(textBox1);
+            b = Convert.ToInt32(textBox2);
+            int c = a * b;
+            textBox3.Text = c.ToString();
+
+        }
+    }
+}
