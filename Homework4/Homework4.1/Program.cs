7dd1aea505835c957115028520b1b84f1b87cc9f
AGYagy <43157278+AGYagy@users.noreply.github.com>
2018-10-08 22:50:43 +0800

    Add files via upload

---
+++ Homework4/Homework4.1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Homework4._1
+{
+    public delegate void AlarmclockEventHandler();
+
+    public class Alarmclock
+    {
+        public event AlarmclockEventHandler SendMessage;
+        public void OnSendMessage()
+        {
+            SendMessage();
+        }
+    }
+
+    public class Student 
+    {
+        public void HandlerEvent()
+        {
+            Console.WriteLine("该开会了");
+        }
+
+        public void Register(Alarmclock e)
+        {
+            Alarmclock alarmclock = new Alarmclock(); 
+            alarmclock.SendMessage += new AlarmclockEventHandler(HandlerEvent);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alarmclock alarmclock = new Alarmclock();
+            Student student = new Student();
+            student.Register(alarmclock);
+            
+
+            if(DateTime.Now.Hour== 6)
+            {
+                alarmclock.OnSendMessage();
+            }
+        }
+    }
+
+    
+    
+}
---
+++ Homework4/Homework4.2/Program.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework4._2
+{
+    class Order
+    {
+        private OrderDetails details;
+        public Order(string number, string item, string costumer)
+        {
+            details = new OrderDetails();
+            details.number = number;
+            details.item = item;
+            details.costumer = costumer;
+        }
+        public void SetData()
+        {
+            string input = Console.ReadLine();
+            Console.Clear();
+
+            int i = 0, j = 0;
+            string[] array = new string[100];
+            string x = null;
+            while (i < input.Length)
+            {
+                if (input[i] != ' ')
+                {
+                    x = x + input[i];
+                }
+                else
+                {
+                    array[j] = x;
+                    j++;
+                    x = null;
+                }
+                i++;
+            }
+            array[j] = x;
+            j++;
+            details.number = array[0];
+            details.item = array[1];
+            details.costumer = array[2];
+        }
+        public string GetNumber()
+        {
+            return details.number;
+        }
+        public string GetCostum()
+        {
+            return details.costumer;
+        }
+        public string GetItem()
+        {
+            return details.item;
+        }
+        public string GetData(int i)
+        {
+            switch (i)
+            {
+                case 0:
+                    return details.number;
+                case 1:
+                    return details.number;
+                case 2:
+                    return details.number;
+            }
+            return null;
+        }
+        public void Output()
+        {
+            Console.WriteLine("Index:" + details.number);
+            Console.WriteLine("Item:" + details.item);
+            Console.WriteLine("Costumer:" + details.costumer);
+        }
+
+    }
+
+    class OrderDetails
+    {
+        public string number;
+        public string item;
+        public string costumer;
+    }
+
+    class OrderService
+    {
+        List<Order> list = new List<Order>();
+
+        public void Add()
+        {
+            string input = Console.ReadLine();
+            Console.Clear();
+
+            int i = 0, j = 0;
+            string[] array = new string[100];
+            string x = null;
+            while (i < input.Length)
+            {
+                if (input[i] != ' ')
+                {
+                    x = x + input[i];
+                }
+                else
+                {
+                    array[j] = x;
+                    j++;
+                    x = null;
+                }
+                i++;
+            }
+            array[j] = x;
+            j++;
+
+            list.Add(new Order(array[0], array[1], array[2]));
+        }
+
+        public void Find()
+        {
+            string input = Console.ReadLine();
+            Console.Clear();
+            foreach (Order var in list)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (var.GetData(i).Contains(input))
+                    {
+                        var.Output();
+                        break;
+                    }
+                }
+            }
+        }
+
+
+        public void Delete()
+        {
+            string input = Console.ReadLine();
+            Console.Clear();
+            foreach (Order var in list)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (var.GetData(i).Contains(input))
+                    {
+                        list.Remove(var);
+                        break;
+                    }
+                }
+            }
+        }
+
+
+        public void Change()
+        {
+            string input = Console.ReadLine();
+            Console.Clear();
+            foreach (Order var in list)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (var.GetData(i).Contains(input))
+                    {
+                        var.SetData();
+                        break;
+                    }
+                }
+            }
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderService orderService = new OrderService();
+
+            int index = Convert.ToInt32(Console.ReadLine());
+            while (index != 0)
+            {
+                switch (index)
+                {
+                    case 1:
+                        Console.Clear();
+                        orderService.Add();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        orderService.Find();
+                        break;
+                }
+                index = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
